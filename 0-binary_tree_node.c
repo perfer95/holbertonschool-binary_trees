b49c76f50c58c286726345f26f7d019246a7fd51
perfer95 <perfer95@outlook.com>
2024-04-10 06:30:52 +0000

    0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_tree - function that creates a binary tree node
+ * binary_tree_node - function that creates a binary tree node
  * @parent: is a pointer to the parent node of the node to create
  * @value: is the value to put in the new node
  * Return: Reurn a node binary_tree_t type
