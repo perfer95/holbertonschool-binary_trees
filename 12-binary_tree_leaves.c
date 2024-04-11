244d892d07ba82076ef0d629424f5fc86d437738
perfer95 <perfer95@outlook.com>
2024-04-11 08:13:47 +0000

    12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -5,7 +5,7 @@
  * @tree: pointer to the root node of the tree to count the number of leaves.
  * Return: a size_t or 0 if tree is NULL.
  */
-size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaves = 0;
 
