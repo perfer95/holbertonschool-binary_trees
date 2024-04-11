df85dc6ea4f104d92f82c0b657da8fa38b5e52b5
perfer95 <perfer95@outlook.com>
2024-04-11 07:08:28 +0000

    7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - function that goes through a binary
+ * binary_tree_inorder - function that goes through a binary
  * tree using in-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node.
