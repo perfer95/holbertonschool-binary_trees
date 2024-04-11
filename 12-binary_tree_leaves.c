f4f4c856bd2ad69836009df8cdf647ffc21a3792
perfer95 <perfer95@outlook.com>
2024-04-11 08:14:43 +0000

    12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,8 +13,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	{
 		if ((tree->left == NULL) && (tree->right == NULL))
 			leaves = leaves + 1;
-		leaves = leaves + binary_tree_size(tree->left);
-		leaves = leaves + binary_tree_size(tree->right);
+		leaves = leaves + binary_tree_leaves(tree->left);
+		leaves = leaves + binary_tree_leaves(tree->right);
 	}
 
 	return (leaves);
