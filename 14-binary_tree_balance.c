54fd36bd6587c631fd213104a88c2c266433717f
perfer95 <perfer95@outlook.com>
2024-04-12 08:30:12 +0000

    14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,8 +32,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	if (tree->left != NULL)
 		left = 1 + binary_tree_height(tree->left);
+	else
+		left = 1;
 	if (tree->right != NULL)
 		right = 1 + binary_tree_height(tree->right);
+	else
+		right = 1;
 
 	if (left > right)
 		return (left);
