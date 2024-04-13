9dcffec1c4123c7d9ad37689c830f3dc0c6db1ba
perfer95 <perfer95@outlook.com>
2024-04-13 06:52:46 +0000

    15

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -20,11 +20,11 @@ int binary_tree_balance(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_height - function that measures the height of a binary tree
+ * binary_tree_height_v2 - function that measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height.
  * Return: a size_t or 0 if tree is NULL
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height_v2(const binary_tree_t *tree)
 {
 	size_t left = 0, right = 0;
 
